515b943f8eb890260602e0495e03ecb79a9a58fa
Matt Deming <vballmatt44@gmail.com>
2022-02-09 20:40:58 -0700

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,7 @@
+# Book Titles
+
+1. Learn Ruby the Hard Way
+2. To Kill a Mockingbird
+3. Hunger Games
+4. Man on Fire
+5. Firemen
---
+++ challenge/pc.rb
@@ -0,0 +1,11 @@
+# We choose pc.rb to stand for "paired challenge".
+
+# Variable 'library' declared and given a value
+library = "Loveland Library"
+books = 1200
+library_open = false
+returned_book = true
+checkout_books = 200
+most_borrowed = "Hitchhiker's Guide to the Galaxy"
+
+sci_fi_books = ["Hunger Games", "Star Wars", "Dragonheart",]
