2e4b0d662a8ca495a1daf929765ffe64f3429405
Matt Deming <vballmatt44@gmail.com>
2022-02-09 20:51:41 -0700

    Finish paired challenge with final remarks

--- challenge/index.md
+++ challenge/index.md
@@ -5,3 +5,14 @@
 3. Hunger Games
 4. Man on Fire
 5. Firemen
+
+## Paired Challenge
+**__Wesley G.__**
+# Workflow
+* We didn't know if this was a Driver/Navigator exercise to start so we
+went Solo but with Wesley sharing his screen.
+* I personally, got stuck with the initial `git clone` as I created a directory
+prematurely. I felt I was in a rush, and I need to slow down and read the
+directions a little closer.
+* Wesley did a good job talking through what he was doing, and since he was
+ahead of me I let him do the "navigating". 
--- challenge/pc.rb
+++ challenge/pc.rb
@@ -1,11 +1,13 @@
 # We choose pc.rb to stand for "paired challenge".
 
-# Variable 'library' declared and given a value
+# Variable 'library' declared and given a value 'Loveland Library'
 library = "Loveland Library"
 books = 1200
+# Variable 'library_open' declared and boolean value given 'true'
 library_open = false
 returned_book = true
+# Variable 'checkout_books' declared and integer given '200'
 checkout_books = 200
 most_borrowed = "Hitchhiker's Guide to the Galaxy"
-
+# Array 'sci_fi_books' declared and elements added
 sci_fi_books = ["Hunger Games", "Star Wars", "Dragonheart",]
